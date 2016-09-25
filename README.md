60d30550463175d5437b1c727e05ec9309df91e2
Grisha K <naspeh@gmail.com>
2016-09-26 01:33:00 +0300

    WIP: simplify main script; some updates

--- README.md
+++ README.md
@@ -7,4 +7,8 @@ positional arguments:
 
 optional arguments:
   -h, --help            show this help message and exit
+
+$ ./sd lxc-base
+$ ./sd i -s root@10.0.3.187 --dev
+$ ssh root@10.0.3.187 "source /opt/repos/.env/bin/activate; C_FORCE_ROOT=1 honcho -d /opt/repos/ start"
 ```
---
+++ bin/lxc_base.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -x
+
+[ -n "$delete" ] && (lxc-stop -n ${name}; lxc-destroy -n ${name})
+
+lxc-create -t download -n ${name} -- -d ubuntu -r xenial -a amd64 &&
+lxc-start -n ${name} &&
+sleep 5 && lxc-attach --clear-env -n ${name} -- /bin/sh -c "
+    DEBIAN_FRONTEND=noninteractive
+    apt-get install -y openssh-server
+" &&
+cat ~/.ssh/id_rsa.pub | lxc-attach -n ${name} -- /bin/sh -c "
+    /bin/mkdir -p /root/.ssh;
+    /bin/cat > /root/.ssh/authorized_keys
+" &&
+echo "ssh root@$$(lxc-info -n ${name} -iH)"
