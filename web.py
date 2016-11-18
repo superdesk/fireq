1c5bab3918ecff81e3cb1f24fa933829c4e95ba3
Grisha K <naspeh@gmail.com>
2016-11-18 04:35:28 +0000

    Run e2e tests in parallel; use logging

--- web.py
+++ web.py
@@ -5,15 +5,70 @@ import datetime as dt
 import hashlib
 import hmac
 import json
+import logging
+import math
 import os
 import re
+import warnings
+from asyncio.subprocess import PIPE
 from pathlib import Path
 
-from aiofiles import open as async_open
+# from aiofiles import open as async_open
 from aiohttp import web, ClientSession
 
+log = logging.getLogger(__name__)
+logging.basicConfig(
+    level=logging.DEBUG,
+    datefmt='%H:%M:%S',
+    format='%(asctime)s %(message)s'
+)
 root = Path(__file__).resolve().parent
 conf = None
+
+
+def init_conf():
+    global conf
+    with open('config.json', 'r') as f:
+        conf = json.loads(f.read())
+
+    defaults = [
+        ('debug', False),
+        ('debug_aio', False),
+        ('sdbase', 'sdbase'),
+        ('domain', 'localhost'),
+        ('logurl', lambda c: 'http://%s/' % c['domain']),
+        ('e2e_count', 4),
+    ]
+    for key, value in defaults:
+        if callable(value):
+            value = value(conf)
+        conf.setdefault(key, value)
+
+
+def init_loop(loop=None):
+    init_conf()
+    if not loop:
+        loop = asyncio.get_event_loop()
+
+    if conf['debug_aio']:
+        # Enable debugging
+        loop.set_debug(True)
+
+        # Make the threshold for "slow" tasks very very small for
+        # illustration. The default is 0.1, or 100 milliseconds.
+        loop.slow_callback_duration = 0.001
+
+        # Report all mistakes managing asynchronous resources.
+        warnings.simplefilter('always', ResourceWarning)
+    return loop
+
+
+def get_app():
+    app = web.Application()
+    init_loop(app.loop)
+    app.router.add_post('/', hook)
+    app.router.add_static('/push', root / 'push', show_index=True)
+    return app
 
 
 def pretty_json(obj):
@@ -31,10 +86,11 @@ async def sh(cmd, ctx, *, logfile=None):
             'exit $code'
             .format(cmd=cmd, path=ctx['logpath'] + logfile)
         )
-    print(cmd)
-    proc = await asyncio.create_subprocess_shell(cmd)
-    code = await proc.wait()
-    print(code, cmd)
+    log.info(cmd)
+    proc = await asyncio.create_subprocess_shell(cmd, stdout=PIPE, stderr=PIPE)
+    out, err = await proc.communicate()
+    code = proc.returncode
+    log.info('code=%s\n%s\nout=%r\nerr=%r', code, cmd, out, err)
     return code
 
 
@@ -81,7 +137,7 @@ def get_ctx(headers, body, **extend):
         prefix = 'sdc' + prefix
         checks = {'targets': ['e2e', 'npmtest', 'docs']}
     else:
-        print('Repository %r is not supported', repo)
+        log.warn('Repository %r is not supported', repo)
         return {}
 
     name = '%s-%s' % (prefix, name)
@@ -115,7 +171,7 @@ def get_ctx(headers, body, **extend):
         logurl=conf['logurl'] + ctx['logpath']
     )
     os.makedirs(ctx['logpath'], exist_ok=True)
-    print(pretty_json(ctx))
+    log.info(pretty_json(ctx))
     return ctx
 
 
@@ -141,18 +197,98 @@ async def post_status(ctx, state=None, extend=None, code=None):
     async with ClientSession(headers=headers) as s:
         async with s.post(ctx['statuses_url'], data=json.dumps(data)) as resp:
             body = pretty_json(await resp.json())
-            print('Posted status: %s' % resp.status)
-            print(body)
+            log.info('Posted status: %s\n%s', resp.status, body)
             if resp.status != 201:
-                print(pretty_json(data))
+                log.warn(pretty_json(data))
             path = '{path}{target}-{state}.json'.format(
                 path=ctx['logpath'],
                 target=data['context'].rsplit('/', 1)[1],
                 state=state
             )
-            async with async_open(path, mode='w') as f:
-                await f.write(body)
+            with open(path, 'w') as f:
+                f.write(body)
+            # async with async_open(path, 'w') as f:
+            #     await f.write(body)
             return resp
+
+
+def chunked(l, n):
+    # inspiration: http://stackoverflow.com/a/24484181
+    chunksize = int(math.ceil(len(l) / n))
+    return (l[i * chunksize:i * chunksize + chunksize] for i in range(n))
+
+
+async def check_e2e(ctx):
+    ctx.update(name_e2e='%s-e2e' % ctx['name_uniq'])
+    code = await sh('./fire lxc-copy -sb {name_uniq} {name_e2e}', ctx)
+    if code != 0:
+        return code
+
+    pattern = '*' * 30
+    cmd = '''
+    cd {r} &&
+    ./fire r -e {endpoint} --lxc-name={name_e2e} -a 'pattern="{p}" do_specs'
+    '''.format(r=root, p=pattern, **ctx)
+    proc = await asyncio.create_subprocess_shell(cmd, stdout=PIPE, stderr=PIPE)
+    out, err = await proc.communicate()
+    if proc.returncode != 0:
+        log.error('ERROR: %s', err)
+        return 1
+    specs = out.decode().rsplit(pattern, 1)[-1].split()
+
+    targets = chunked(specs, conf['e2e_count'])
+    targets = [
+        {
+            'target': 'e2e--part%s' % num,
+            'parent': 'e2e',
+            'env': 'specs=%s' % ','.join(t)
+        } for num, t in enumerate(targets, 1)
+    ]
+    code = await run_targets(ctx, targets)
+    return code
+
+
+async def run_target(ctx, target):
+    cmd = '''
+    lxc={name_uniq}-{t};
+    ./fire lxc-copy {clean} -s -b {name_uniq} $lxc
+    ./fire r --lxc-name=$lxc --env="{env}" -e {endpoint} -a "{p}=1 do_checks"
+    '''
+
+    if isinstance(target, dict):
+        parent = target['parent']
+        env = target['env']
+        target = target['target']
+    else:
+        parent, env = target, ''
+    env = ' '.join(i for i in (env, ctx['env']) if i)
+
+    logfile = 'check-%s.log' % target
+    status = {
+        'target_url': ctx['logurl'] + logfile,
+        'context': 'naspeh-sf/deploy/check-%s' % target
+    }
+    await post_status(ctx, 'pending', extend=status)
+    if target == 'e2e':
+        code = await check_e2e(dict(ctx, logfile=logfile))
+    else:
+        c = dict(ctx, p=parent, t=target, env=env)
+        code = await sh(cmd, c, logfile=logfile)
+    await post_status(ctx, code=code, extend=status)
+    log.info('Finished %r with %s', target, code)
+    return code
+
+
+async def wait_for(proces):
+    failed = 0
+    for f in asyncio.as_completed(proces):
+        failed = await f or failed
+    return failed
+
+
+async def run_targets(ctx, targets):
+    proces = [run_target(dict(ctx), t) for t in targets]
+    return await wait_for(proces)
 
 
 async def checks(ctx):
@@ -166,44 +302,20 @@ async def checks(ctx):
 
     targets = ctx['checks']['targets']
     env = ctx['checks'].get('env', '')
+    env = ' '.join(i for i in (env, ctx['env']) if i)
 
     if ctx['install']:
         code = await sh('''
         ./fire lxc-copy -s -b {sdbase} {clean} {name_uniq}
         ./fire i --lxc-name={name_uniq} --env="{env}" -e {endpoint};
         lxc-stop -n {name_uniq};
-        ''', dict(ctx, env='%s %s' % (env, ctx['env'])))
+        ''', dict(ctx, env=env))
 
         if code:
             await clean(code)
-            return
+            return code
 
-    cmd = '''
-    lxc={name_uniq}-{t};
-    ./fire lxc-copy -s -b {name_uniq} $lxc
-    ./fire r --lxc-name=$lxc --env="{env}" -e {endpoint} -a "{t}=1 do_checks"
-    '''
-
-    async def run(target):
-        logfile = 'check-%s.log' % target
-        status = {
-            'target_url': ctx['logurl'] + logfile,
-            'context': 'naspeh-sf/deploy/check-%s' % target
-        }
-        await post_status(ctx, 'pending', extend=status)
-        code = await sh(
-            cmd.format(t=target, **ctx),
-            ctx, logfile=logfile
-        )
-        await post_status(ctx, code=code, extend=status)
-        print('Finished %r with %s' % (target, code))
-        return code
-
-    proces = [run(t) for t in targets]
-    code = 0
-    for f in asyncio.as_completed(proces):
-        code = await f or 1
-
+    code = await run_targets(ctx, targets)
     await clean(code)
     return code
 
@@ -232,10 +344,7 @@ async def pubweb(ctx):
 async def build(ctx):
     await post_status(ctx, 'pending')
     proces = [t(ctx) for t in (pubweb, checks)]
-    failed = 0
-    for f in asyncio.as_completed(proces):
-        failed = await f or failed
-    return failed
+    return await wait_for(proces)
 
 
 def get_signature(body):
@@ -255,41 +364,17 @@ async def hook(request):
     body = await request.json()
     headers = dict(request.headers.items())
     del headers['X-Hub-Signature']
-    print(pretty_json(headers))
-    print(pretty_json(body))
+    log.info('%s\n\n%s', pretty_json(headers), pretty_json(body))
     ctx = get_ctx(headers, body, clean=True)
     if ctx:
         os.makedirs(ctx['logpath'], exist_ok=True)
-        async with async_open(ctx['path'] + '/request.json', mode='w') as f:
-            await f.write(pretty_json([headers, body]))
+        with open(ctx['path'] + '/request.json', 'w') as f:
+            f.write(pretty_json([headers, body]))
+        # async with async_open(ctx['path'] + '/request.json', 'w') as f:
+        #     await f.write(pretty_json([headers, body]))
 
         request.app.loop.create_task(build(ctx))
     return web.json_response('OK')
-
-
-def init_conf():
-    global conf
-    with open('config.json', 'r') as f:
-        conf = json.loads(f.read())
-
-    defaults = [
-        ('debug', False),
-        ('sdbase', 'sdbase'),
-        ('domain', 'localhost'),
-        ('logurl', lambda c: 'http://%s/' % c['domain']),
-    ]
-    for key, value in defaults:
-        if callable(value):
-            value = value(conf)
-        conf.setdefault(key, value)
-
-
-def get_app():
-    init_conf()
-    app = web.Application()
-    app.router.add_post('/', hook)
-    app.router.add_static('/push', root / 'push', show_index=True)
-    return app
 
 
 if __name__ == '__main__':
