ae86ad1cfbff7f8aa9b993ee7129dd7703217b81
Grisha K <naspeh@gmail.com>
2017-01-05 16:47:01 +0000

    Fix user installation

--- files/superdesk-10.sh
+++ files/superdesk-10.sh
@@ -5,13 +5,14 @@ repo=/opt/superdesk-deploy
 apt-get update
 apt-get -y install git
 
+rm -rf $repo
 git clone --depth 1 https://github.com/naspeh-sf/deploy.git $repo
 
 cd $repo; ./fire i -e superdesk/10 --services --prepopulate
 
 echo "*********************************************************************************"
 echo "Installation complete!"
-echo "- Open in a bowser the address: http://$(curl -s http://whatismyip.akamai.com/)/"
+echo "- Open in a bowser the address: http://your_server_address/"
 echo "- To login use default credentials:"
 echo "    Login: admin"
 echo "    Password: admin"
