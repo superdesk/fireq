ec7efbd67122889869b4c76d600f11dbfbb1f50a
Grisha K <naspeh@gmail.com>
2017-02-20 19:35:50 +0000

    Fix tests

--- tests/test_statuses.py
+++ tests/test_statuses.py
@@ -43,8 +43,8 @@ def test_base(gh_call, sp, main, raises):
 
     gh_call.reset_mock()
     main('ci sd master -t www')
-    assert gh_call.call_count == 3
-    a1, a2, a3 = gh_call.call_args_list
+    assert gh_call.call_count == 6
+    a1, a2, a3, a4, a5, a6 = gh_call.call_args_list
     assert a1[0] == (
         'repos/superdesk/superdesk/statuses/<sha>',
         {
@@ -57,13 +57,40 @@ def test_base(gh_call, sp, main, raises):
     assert a2[0] == (
         'repos/superdesk/superdesk/statuses/<sha>',
         {
+            'description': 'click "Details" to restart the build',
+            'target_url': 'http://localhost/sd/heads/master/restart',
+            'context': 'fire:restart',
+            'state': 'success'
+        }
+    )
+    assert a3[0] == (
+        'repos/superdesk/superdesk/statuses/<sha>',
+        {
+            'description': '',
+            'target_url': logs + '-sd-master/build.log',
+            'context': 'fire:build',
+            'state': 'pending'
+        }
+    )
+    assert a4[0] == (
+        'repos/superdesk/superdesk/statuses/<sha>',
+        {
+            'description': 'duration: 0m0s',
+            'target_url': logs + '-sd-master/build.log.htm',
+            'context': 'fire:build',
+            'state': 'success'
+        }
+    )
+    assert a5[0] == (
+        'repos/superdesk/superdesk/statuses/<sha>',
+        {
             'description': '',
             'target_url': logs + '-sd-master/www.log',
             'context': 'fire:www',
             'state': 'pending'
         }
     )
-    assert a3[0] == (
+    assert a6[0] == (
         'repos/superdesk/superdesk/statuses/<sha>',
         {
             'description': 'click "Details" to see the test instance',
@@ -77,8 +104,8 @@ def test_base(gh_call, sp, main, raises):
     sp.call.return_value = 16
     with raises(SystemExit):
         main('ci sd master -t www')
-    assert gh_call.call_count == 3
-    a1, a2, a3 = gh_call.call_args_list
+    assert gh_call.call_count == 4
+    a1, a2, a3, a4 = gh_call.call_args_list
     assert a1[0] == (
         'repos/superdesk/superdesk/statuses/<sha>',
         {
@@ -91,18 +118,27 @@ def test_base(gh_call, sp, main, raises):
     assert a2[0] == (
         'repos/superdesk/superdesk/statuses/<sha>',
         {
-            'description': '',
-            'target_url': logs + '-sd-master/www.log',
-            'context': 'fire:www',
-            'state': 'pending'
+            'description': 'click "Details" to restart the build',
+            'target_url': 'http://localhost/sd/heads/master/restart',
+            'context': 'fire:restart',
+            'state': 'success'
         }
     )
     assert a3[0] == (
         'repos/superdesk/superdesk/statuses/<sha>',
         {
+            'description': '',
+            'target_url': logs + '-sd-master/build.log',
+            'context': 'fire:build',
+            'state': 'pending'
+        }
+    )
+    assert a4[0] == (
+        'repos/superdesk/superdesk/statuses/<sha>',
+        {
             'description': 'duration: 0m0s',
-            'target_url': logs + '-sd-master/www.log.htm',
-            'context': 'fire:www',
+            'target_url': logs + '-sd-master/build.log.htm',
+            'context': 'fire:build',
             'state': 'failure'
         }
     )
@@ -147,29 +183,27 @@ def test_no_statuses(main, raises, capsys, gh_call):
         assert not gh_call.called
 
 
-def test_base__real_http(sp, capfd, real_http, gh_call, main):
-    gh_call._stop()
-    with patch.dict('fireq.cli.conf', {'no_statuses': False}):
-        main('ci sd naspeh')
-        out, err = capfd.readouterr()
-        #  1: clean "!restart" is not presented anymore
-        #  2: success "restart"
-        #  3, 4, 5: waiting with general log directory
-        #  6, 7, 8, 9: pending with particular log file
-        # 10,11,12,13: success
-        assert 13 == err.count("201 url='https://api.github.com/repos/")
-
-    # no statuses
-    main('ci sd naspeh')
-    out, err = capfd.readouterr()
-    assert 0 == err.count("201 url='https://api.github.com/repos/")
-
-
 @patch.dict('fireq.cli.conf', {'no_statuses': False})
 @patch('fireq.gh.get_sha', lambda *a: '<sha>')
 def test_cleaning(gh_call, main, load_json):
     gh_call.return_value = load_json('status-sdcpr_1282.json')
     main('ci sdc pull/1282')
+
+    s = [
+        (i[0][1]['context'], i[0][1]['state'])
+        for i in gh_call.call_args_list[1:]
+    ]
+    assert s == [
+        ('fire:check-docs', 'success'),
+        ('fire:check-e2e', 'success'),
+        ('fire:!restart', 'success'),
+        ('fire:check-e2e--part2', 'pending'),
+        ('fire:restart', 'success'),
+        ('fire:build', 'pending'),
+        ('fire:build', 'success'),
+        ('fire:check-e2e--part2', 'pending'),
+        ('fire:check-e2e--part2', 'success')
+    ]
 
     def call_args(context):
         return ('repos/superdesk/superdesk-client-core/statuses/<sha>', {
@@ -179,8 +213,8 @@ def test_cleaning(gh_call, main, load_json):
             'state': 'success'
         })
 
-    assert 7 == gh_call.call_count
-    a1, a2, a3, a4, a5, a6, a7 = gh_call.call_args_list
+    assert 10 == gh_call.call_count
+    a1, a2, a3, a4, a5, a6, a7, a8, a9, a10 = gh_call.call_args_list
     assert a1[0] == (
         'repos/superdesk/superdesk-client-core/commits/<sha>/status',
     )
@@ -199,13 +233,40 @@ def test_cleaning(gh_call, main, load_json):
     assert a6[0] == (
         'repos/superdesk/superdesk-client-core/statuses/<sha>',
         {
+            'description': 'click "Details" to restart the build',
+            'target_url': 'http://localhost/sdc/pull/1282/restart',
+            'context': 'fire:restart',
+            'state': 'success'
+        }
+    )
+    assert a7[0] == (
+        'repos/superdesk/superdesk-client-core/statuses/<sha>',
+        {
+            'description': '',
+            'state': 'pending',
+            'context': 'fire:build',
+            'target_url': logs + '-sdcpr-1282/build.log',
+        }
+    )
+    assert a8[0] == (
+        'repos/superdesk/superdesk-client-core/statuses/<sha>',
+        {
+            'description': 'duration: 0m0s',
+            'state': 'success',
+            'context': 'fire:build',
+            'target_url': logs + '-sdcpr-1282/build.log.htm',
+        }
+    )
+    assert a9[0] == (
+        'repos/superdesk/superdesk-client-core/statuses/<sha>',
+        {
             'description': '',
             'state': 'pending',
             'context': 'fire:check-e2e--part2',
             'target_url': logs + '-sdcpr-1282/check-e2e--part2.log',
         }
     )
-    assert a7[0] == (
+    assert a10[0] == (
         'repos/superdesk/superdesk-client-core/statuses/<sha>',
         {
             'description': 'duration: 0m0s',
