86f0a42f30b13b3d2b55f0439f04f3d1052c58b0
Grisha K <naspeh@gmail.com>
2017-02-24 13:09:00 +0000

    Use hostname match in "init/lb.sh" to set "SYNDICATION" var

--- tpl/init/lb-syndtest2.sh
+++
@@ -1,5 +0,0 @@
-{{>init/lb.sh}}
-
-cat <<"EOF" >> {{config}}
-SYNDICATION=true
-EOF
--- tpl/init/lb.sh
+++ tpl/init/lb.sh
@@ -1,3 +1,7 @@
 cat <<"EOF" >> {{config}}
 {{>init/.liveblog.sh}}
 EOF
+
+[[ "$(hostname)" =~ ^lb-syndtest ]] && cat <<"EOF" >> {{config}}
+SYNDICATION=true
+EOF
