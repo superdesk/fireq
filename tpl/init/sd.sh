7dee2d9993aa05ce9016f9160141cac156fa5d08
Grisha K <naspeh@gmail.com>
2017-02-24 12:12:48 +0000

    Define init step for sd-naspeh in general "init/sd.sh"

--- tpl/init/sd.sh
+++ tpl/init/sd.sh
@@ -1,3 +1,8 @@
 cat <<"EOF" >> {{config}}
 {{>init/.superdesk.sh}}
 EOF
+
+if [[ "$(hostname)" =~ ^sd-naspeh ]]; then
+    _activate
+    pip install -e git+https://github.com/superdesk/superdesk-core.git@1.6#egg=Superdesk-Core
+fi
