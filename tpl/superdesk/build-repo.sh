42668bc1f77180fad7b68b91849217a7c93b9045
Grisha K <naspeh@gmail.com>
2017-03-09 15:35:42 +0000

    Add an option to specify "repo_ref" for install scripts

--- tpl/superdesk/build-repo.sh
+++ tpl/superdesk/build-repo.sh
@@ -5,19 +5,17 @@ cd $repo
 if [ ! -d $repo/.git ]; then
     git init
     git remote add origin {{repo_remote}}
+    repo_ref=${repo_ref:-'{{repo_ref}}'}
+    {{#is_pr}}
+    git fetch origin $repo_ref/merge: || git fetch origin $repo_ref/head:
+    # TODO: use latest sha for now
+    git checkout FETCH_HEAD
+    {{/is_pr}}
+    {{^is_pr}}
+    repo_sha={{repo_sha}}
+    git fetch origin $repo_ref:
+    git checkout ${repo_sha:-FETCH_HEAD}
+    unset repo_sha
+    {{/is_pr}}
+    unset repo repo_ref
 fi
-
-cd $repo
-repo_ref={{repo_ref}}
-{{#is_pr}}
-git fetch origin $repo_ref/merge: || git fetch origin $repo_ref/head:
-# TODO: use latest sha for now
-git checkout FETCH_HEAD
-{{/is_pr}}
-{{^is_pr}}
-repo_sha={{repo_sha}}
-git fetch origin $repo_ref:
-git checkout ${repo_sha:-FETCH_HEAD}
-unset repo_sha
-{{/is_pr}}
-unset repo repo_ref
