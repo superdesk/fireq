3266eac15ffb74ff54fb2ab839e30e52db6c76ce
Grisha K <naspeh@gmail.com>
2017-04-20 15:53:28 +0000

    Don't use memory as storage for databases

--- tpl/superdesk/testing.sh
+++ tpl/superdesk/testing.sh
@@ -1,20 +1,4 @@
 {{#db_local}}
-mongo_init='path=/tmp/mongodb; [ -d $path ] || mkdir $path; chown mongodb:mongodb $path'
-cat <<EOF > /etc/systemd/system/mongo-init.service
-[Unit]
-Description=Mongo init
-Before=local-fs.target
-
-[Service]
-Type=oneshot
-ExecStart=/bin/sh -c "$mongo_init"
-
-[Install]
-RequiredBy=local-fs.target
-EOF
-systemctl enable mongo-init
-systemctl start mongo-init
-
 # tune elasticsearch
 cat <<EOF > /etc/elasticsearch/elasticsearch.yml
 network.bind_host: 0.0.0.0
@@ -22,7 +6,7 @@ node.local: true
 discovery.zen.ping.multicast: false
 path.repo: /var/tmp/elasticsearch
 index.number_of_replicas: 0
-index.store.type: memory
+#index.store.type: memory
 #index.refresh_interval: 30s
 
 # Next setting break behave tests
@@ -33,7 +17,7 @@ echo 'log4j.rootLogger=OFF' > /etc/elasticsearch/logging.yml
 # tune mongo
 cat <<EOF > /etc/mongod.conf
 storage:
-  dbPath: /tmp/mongodb
+  dbPath: /var/lib/mongodb
   journal:
     enabled: false
   engine: wiredTiger
