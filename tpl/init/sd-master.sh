4b61aa92bfa05dc3d8278431d7d8f448cfbb0be4
Grisha K <naspeh@gmail.com>
2017-04-28 14:37:14 +0000

    Use relative media urls for sd-master

--- tpl/init/sd-master.sh
+++ tpl/init/sd-master.sh
@@ -11,4 +11,17 @@ EOF
 
 cat <<"EOF" >> {{config}}
 DB_HOST=data-sd--elastic2
+DB_NAME=sd-master--2
+
+{{>init/.amazon.sh}}
+AMAZON_CONTAINER_NAME='sd-frankfurt-test'
+AMAZON_REGION=eu-central-1
+# relative media urls
+MEDIA_PREFIX=/sd-master
+EOF
+
+cat <<"EOF" > /etc/nginx/conf.d/media.inc
+location /sd-master/ {
+    return 302 https://sd-frankfurt-test.s3-eu-central-1.amazonaws.com$request_uri;
+}
 EOF
--- tpl/init/sd-naspeh.sh
+++ tpl/init/sd-naspeh.sh
@@ -1,10 +1,20 @@
 {{>init/sd.sh}}
 
 cat <<"EOF" >> {{config}}
-# use bucket from Frankfurt
+DB_HOST=data-sd--elastic2
+DB_NAME=sd-naspeh--2
+
 {{>init/.amazon.sh}}
 AMAZON_CONTAINER_NAME='sd-frankfurt-test'
 AMAZON_REGION=eu-central-1
+# relative media urls
+MEDIA_PREFIX=/sd-naspeh
+EOF
+
+cat <<"EOF" > /etc/nginx/conf.d/media.inc
+location /sd-naspeh/ {
+    return 302 https://sd-frankfurt-test.s3-eu-central-1.amazonaws.com$request_uri;
+}
 EOF
 
 set +x
