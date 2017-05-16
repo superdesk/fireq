5f1a078065bfc7cdc137418469730ae1204576f2
Grisha K <naspeh@gmail.com>
2017-05-16 21:54:03 +0000

    Use dev mail for sd-thesource

--- tpl/init/sd-thesource.sh
+++ tpl/init/sd-thesource.sh
@@ -3,7 +3,7 @@
 {{>init/.amazon.sh}}
 
 cat <<EOF >> {{config}}
-MAIL_SERVER=10.0.3.1
+#MAIL_SERVER=10.0.3.1
 
 {{>init/.amazon.sh}}
 MEDIA_PREFIX=https://$AMAZON_CONTAINER_NAME.s3-$AMAZON_REGION.amazonaws.com/$AMAZON_S3_SUBFOLDER
