2c8a2164c6f8e6e1277a3438163b73d2370c5190
Grisha K <naspeh@gmail.com>
2017-05-31 08:58:12 +0000

    Fix MEDIA_PREFIX for sd-thesource

--- sd-thesource.sh
+++ sd-thesource.sh
@@ -6,5 +6,5 @@ cat <<EOF >> {{config}}
 MAIL_SERVER=10.0.3.1
 
 {{>init/.amazon.sh}}
-MEDIA_PREFIX=https://$AMAZON_CONTAINER_NAME.s3-$AMAZON_REGION.amazonaws.com/$AMAZON_S3_SUBFOLDER
+MEDIA_PREFIX=https://$AMAZON_CONTAINER_NAME.s3-$AMAZON_REGION.amazonaws.com/sd-thesource
 EOF
