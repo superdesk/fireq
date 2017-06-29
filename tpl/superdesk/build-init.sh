6dc0f5b9bb020e958dcd33c6e3508fecdd70d30b
Grisha K <naspeh@gmail.com>
2017-06-29 15:24:21 +0000

    Add libxmlsec1-dev to build python3-saml (fidelity)

--- tpl/superdesk/build-init.sh
+++ tpl/superdesk/build-init.sh
@@ -7,7 +7,8 @@ build-essential libffi-dev \
 libtiff5-dev libjpeg8-dev zlib1g-dev \
 libfreetype6-dev liblcms2-dev libwebp-dev \
 curl libfontconfig libssl-dev \
-libxml2-dev libxslt1-dev
+libxml2-dev libxslt1-dev \
+libxmlsec1-dev
 
 {{>add-node.sh}}
 
