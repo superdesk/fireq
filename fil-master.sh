18b035e441d0ff86801a541c1240fd2a60913d2c
Grisha K <naspeh@gmail.com>
2017-09-11 16:38:32 +0000

    Use Amazon S3 for `fil-master`; and some minor fix and cleanup

--- fil-master.sh
+++ fil-master.sh
@@ -1,5 +1,6 @@
 {{>init/sd.sh}}
 
+
 cat <<"EOF" >> {{config}}
 {{>init/.amazon.sh}}
 AMAZON_CONTAINER_NAME='sd-frankfurt-test'
@@ -9,6 +10,6 @@ EOF
 cat <<"EOF" > /etc/nginx/conf.d/media.inc
 # serve directly from amazon
 location /api/upload-raw/ {
-    rewrite ^/api/upload-raw/(.*)$ https://sd-frankfurt-test.s3-eu-central-1.amazonaws.com/sds-naspeh/$1;
+    rewrite ^/api/upload-raw/(.*)$ https://sd-frankfurt-test.s3-eu-central-1.amazonaws.com/fil-master/$1;
 }
 EOF
--- sdspr-900.sh
+++
@@ -1,5 +0,0 @@
-{{>init/sd.sh}}
-
-cat <<"EOF" >> {{config}}
-NEW_RELIC_LICENSE_KEY=
-EOF
