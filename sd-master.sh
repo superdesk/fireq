21edaf95074157613cb90e863f182bfc48564cce
Grisha K <naspeh@gmail.com>
2017-09-21 18:56:37 +0000

    Update init config for "sd-master"

--- sd-master.sh
+++ sd-master.sh
@@ -10,17 +10,14 @@ window.superdeskConfig = {
 EOF
 
 cat <<"EOF" >> {{config}}
-DB_HOST=data-sd--elastic2
-
 {{>init/.amazon.sh}}
 AMAZON_CONTAINER_NAME='sd-frankfurt-test'
 AMAZON_REGION=eu-central-1
-# relative media urls
-MEDIA_PREFIX=/sd-master
 EOF
 
 cat <<"EOF" > /etc/nginx/conf.d/media.inc
-location /sd-master/ {
-    return 302 https://sd-frankfurt-test.s3-eu-central-1.amazonaws.com$request_uri;
+# serve directly from amazon
+location /api/upload-raw/ {
+    rewrite ^/api/upload-raw/(.*)$ https://sd-frankfurt-test.s3-eu-central-1.amazonaws.com/sd-master/$1;
 }
 EOF
