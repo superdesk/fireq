f838323545ea72dbaed229cadb3a6ab8f0b20514
Grisha K <naspeh@gmail.com>
2017-10-03 11:45:54 +0000

    Liveblog: syndication and marketplace are enabled by default

--- lb-release330.sh
+++
@@ -1,7 +0,0 @@
-{{>init/lb.sh}}
-
-cat <<EOF >> {{config}}
-SYNDICATION=true
-MARKETPLACE=true
-MARKETPLACE_APP_URL=https://market.liveblog.pro/api
-EOF
--- lb.sh
+++ lb.sh
@@ -1,3 +1,7 @@
 cat <<"EOF" >> {{config}}
 {{>init/.liveblog.sh}}
+
+SYNDICATION=true
+MARKETPLACE=true
+MARKETPLACE_APP_URL=https://market.liveblog.pro/api
 EOF
