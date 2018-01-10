b655958f6fbcb89d06da6742e8a1de6bc9245b46
Petr Jasek <jasekpetr@gmail.com>
2018-01-10 11:10:24 +0100

    fix npm tests failing on chrome

--- tpl/superdesk-client/check-npmtest.sh
+++ tpl/superdesk-client/check-npmtest.sh
@@ -1,6 +1,6 @@
 # From: http://cvuorinen.net/2017/05/running-angular-tests-in-headless-chrome/
 # > Without a remote debugging port, Google Chrome exits immediately.
-chrome_opts='--remote-debugging-port=9222'
+chrome_opts='--remote-debugging-port=9222 --no-sandbox'
 {{>add-chrome.sh}}
 
 cd {{repo_client}}
