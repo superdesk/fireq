643191d9d4a43ce49334c0aabbdf49194d16829f
Petr Jašek <petr.jasek@sourcefabric.org>
2018-06-20 14:31:54 +0200

    fix superdesk-analytics build
    it was removing trailing `,` from package.json which broke npm install

--- tpl/superdesk-analytics/build-src.sh
+++ tpl/superdesk-analytics/build-src.sh
@@ -2,7 +2,7 @@
 
 cd {{repo}}
 sed -i 's/.*superdesk-analytics.git.*/-e ..\/analytics/' server/requirements.txt
-sed -i -re 's/("superdesk-analytics":)[^.]*(,?)/\1 "file:..\/analytics"\2/' client/package.json
+sed -i -re 's/("superdesk-analytics":)[^,]*(,?)/\1 "file:..\/analytics"\2/' client/package.json
 cat server/requirements.txt
 cat client/package.json
 
