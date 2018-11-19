e397e31c8f8ecdab9b0c65bb4937bea03630b38c
Ioan-Vasile Pocol <ivp@ivp.local>
2018-11-19 15:44:50 +0200

    fix(upgrade): Execute upgrade scripts on deployments

--- tpl/superdesk/prepopulate.sh
+++ tpl/superdesk/prepopulate.sh
@@ -15,6 +15,7 @@ if _missing_db; then
     python manage.py users:create -u admin -p admin -e 'admin@example.com' --admin
 else
     python manage.py app:initialize_data
+    python manage.py data:upgrade
 fi
 {{/test_data}}
 {{#test_data}}
