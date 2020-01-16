3099153ae4842602bf7d7d49e3192f9ff8d67530
Petr <petr.jasek@sourcefabric.org>
2020-01-16 13:19:19 +0100

    fix expired chrome key preventing update

--- tpl/add-chrome.sh
+++ tpl/add-chrome.sh
@@ -2,9 +2,11 @@
 # we use chrome in headless mode, so no need for xvfb
 
 if [ ! -r /etc/apt/sources.list.d/google.list ]; then
-    curl -s https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
     echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list
 fi
+
+# always fetch the key to avoid expiry
+curl -s https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
 
 apt-get update
 apt-get install -y --no-install-recommends google-chrome-stable
