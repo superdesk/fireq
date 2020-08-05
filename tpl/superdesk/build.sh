16e3d5c22bcb6b7a5876d42781d072d292103ed0
Mark Pittaway <mark.pittaway@gmail.com>
2020-08-05 22:51:40 +1000

    Build SAMS after installing superdesk-core

--- tpl/superdesk/build.sh
+++ tpl/superdesk/build.sh
@@ -3,11 +3,11 @@
 
 {{>build-src.sh}}
 
-{{>build-sams.sh}}
-
 cd {{repo_server}}
 [ -f dev-requirements.txt ] && req=dev-requirements.txt || req=requirements.txt
 time pip install -Ur $req
+
+{{>build-sams.sh}}
 
 cd {{repo_client}}
 time npm install --unsafe-perm
