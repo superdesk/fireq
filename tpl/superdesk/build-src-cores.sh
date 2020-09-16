9ddb61cb18d593437dd336f6fffb2472e5d413f9
Mark Pittaway <mark.pittaway@gmail.com>
2020-09-17 09:33:30 +1000

    [SDESK-5543] Ability to override superdesk branch from fireq.json

--- tpl/superdesk/build-src-cores.sh
+++ tpl/superdesk/build-src-cores.sh
@@ -2,6 +2,11 @@ repo={{repo}}
 github=https://github.com/superdesk
 branch=${branch:-master}
 
+# Overrides the `branch` if it is set in the `.fireq.json` file
+if [ -f {{fireq_json}} ] && [ `jq ".superdesk_branch?" {{fireq_json}}` != "null" ]; then
+    branch=`jq ".superdesk_branch?" {{fireq_json}}`
+fi
+
 [ -d $repo ] || mkdir $repo
 cd $repo
 if [ ! -d $repo/.git ]; then
