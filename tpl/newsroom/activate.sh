59e2975404bacc1f3ecf4921cbc6526c00887ae1
olegpshenichniy <olegpshenichniy@gmail.com>
2020-09-22 16:10:06 +0200

    Newshub elastic search 7

---
+++ tpl/newsroom/activate.sh
@@ -0,0 +1,45 @@
+. {{repo_env}}/bin/activate
+
+set -a
+LC_ALL=en_US.UTF-8
+PYTHONUNBUFFERED=1
+PATH=node_modules/.bin/:$PATH
+
+[ ! -f {{config}} ] || . {{config}}
+
+NEWSROOM_SETTINGS=settings.py
+NEWSROOM_WEBSOCKET_URL="ws{{^is_pr}}s{{/is_pr}}://$HOST/ws"
+NOTIFICATION_KEY="newsroom"
+RECAPTCHA_PUBLIC_KEY="$RECAPTCHA_PUBLIC_KEY"
+RECAPTCHA_PRIVATE_KEY="$RECAPTCHA_PRIVATE_KEY"
+GOOGLE_MAPS_KEY="AIzaSyC14_pEv1mUFFDfUA2zNEzij3RFTcJk5wM"
+SECRET_KEY=$DB_NAME
+
+# mongo
+MONGO_URI="mongodb://$DB_HOST/$DB_NAME"
+CONTENTAPI_MONGO_URI="mongodb://$DB_HOST/$DB_NAME"
+
+# elastic
+# 9200 is elastic 2.4, 9201 is elastic 7
+_ELASTIC_PORT=${ELASTIC_PORT:-'9200'}
+
+if [ -f {{fireq_json}} ] && [ `jq ".elastic?" {{fireq_json}}` -eq 7 ]; then
+    _ELASTIC_PORT=9201
+fi
+ELASTICSEARCH_URL="http://$DB_HOST:$_ELASTIC_PORT"
+CONTENTAPI_ELASTIC_INDEX=$DB_NAME
+CONTENTAPI_ELASTICSEARCH_INDEX=$DB_NAME
+
+# redis and celery
+REDIS_URL=redis://localhost:6379/1
+CELERY_BROKER_URL="$REDIS_URL"
+
+{{#is_pr}}
+MONGO_URI="mongodb://$DB_HOST/nr-master"
+CONTENTAPI_ELASTIC_INDEX=nr-master
+CONTENTAPI_ELASTICSEARCH_INDEX=nr-master
+CONTENTAPI_MONGO_URI="mongodb://$DB_HOST/nr-master"
+NEWS_API_ENABLED=true
+{{/is_pr}}
+
+set +a
--- tpl/newsroom/prepopulate.sh
+++ tpl/newsroom/prepopulate.sh
@@ -1,13 +1,16 @@
 _missing_db() {
-    curl -sI $ELASTICSEARCH_URL/$CONTENTAPI_ELASTICSEARCH_INDEX | grep -q 404
+    ! { curl -sI "$ELASTICSEARCH_URL/${CONTENTAPI_ELASTICSEARCH_INDEX}_items" | grep -q 404 && curl -sI $ELASTICSEARCH_URL/$CONTENTAPI_ELASTICSEARCH_INDEX | grep -q 404 ;}
 }
 
 _activate
 
 if _missing_db; then
-  curl -s -XPUT $ELASTICSEARCH_URL/$CONTENTAPI_ELASTICSEARCH_INDEX
   cd {{repo}}
   set +e
-  python manage.py create_user admin@example.com admin Admin Admin true;
+  python manage.py elastic_rebuild
+  python manage.py create_user admin@example.com admin Admin Admin true
   set -e
+else
+  cd {{repo}}
+  python manage.py data_upgrade
 fi
