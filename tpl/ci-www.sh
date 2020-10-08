0071e2cc6aaee5645e8c0a835bd0e6fcf681ea65
Petr Jašek <jasekpetr@gmail.com>
2020-10-08 18:09:43 +0200

    Revert "make www fail on error (#81)" (#82)
    This reverts commit 86a93663d9e05c05a4e234a07475227dded4b3df.

--- tpl/ci-www.sh
+++ tpl/ci-www.sh
@@ -4,26 +4,11 @@ lxc-destroy -fn $lxc || true
 lxc-copy -s -n {{lxc_build}} -N $lxc
 ./fire lxc-wait --start $lxc
 
-deploy() {
-    {{>ci-deploy.sh}}
-}
-
-# workaround to stop deploy on error but let the script continue
-# so it will destroy the old container and make the error visible
-error=0
-deploy &
-wait $! || {
-    error=$?
-}
+{{>ci-deploy.sh}}
 
 lxc-stop -n $lxc
 lxc-destroy -fn {{uid}} || true
-
-if [ "$error" -eq "0" ]; then
-    lxc-copy -n $lxc -N {{uid}} -R
-else
-    exit $error
-fi
+lxc-copy -n $lxc -N {{uid}} -R
 
 # mount logs directory
 mkdir -p {{host_logs}}
