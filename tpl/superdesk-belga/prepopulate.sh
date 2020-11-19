234cdde280d118c770fae580fa61f9850dc549dc
olegpshenichniy <olegpshenichniy@gmail.com>
2020-11-19 11:38:12 +0100

    Use proper manage.py commands order during deploy.

--- tpl/superdesk-belga/prepopulate.sh
+++ tpl/superdesk-belga/prepopulate.sh
@@ -7,7 +7,7 @@ if _missing_db; then
     python manage.py app:initialize_data
     python manage.py users:create -u admin -p admin -e 'admin@example.com' --admin
 else
-    python manage.py app:initialize_data
     python manage.py data:upgrade
+    python manage.py app:initialize_data
 fi
 {{/test_data}}
