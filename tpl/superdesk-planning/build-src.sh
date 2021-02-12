8f07df3ea57476fe4b37385d7294b69424595ff4
Mark Pittaway <mark.pittaway@gmail.com>
2021-02-12 17:16:56 +1100

    Planning: Support superdesk_branch json config

--- tpl/superdesk-planning/build-src.sh
+++ tpl/superdesk-planning/build-src.sh
@@ -1,5 +1,9 @@
-branch=planning-mvp
-{{>superdesk/build-src-cores.sh}}
-
+# Download the Planning repo first
+# So the fireq.json file is available
 repo={{repo}}/planning
 {{>superdesk/build-src-repo.sh}}
+
+# Use the config `superdesk_branch` if set
+# Otherwise default to `planning-mvp`
+branch=`_get_config_value superdesk_branch planning-mvp
+{{>superdesk/build-src-cores.sh}}
