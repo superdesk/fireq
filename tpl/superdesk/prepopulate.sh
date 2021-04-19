4ce5d3f6cd6117ccd089639c6d8f28864dde1e18
Petr Jasek <jasekpetr@gmail.com>
2021-04-19 12:39:19 +0200

    fix superdesk app init also when not using test data

--- tpl/superdesk/prepopulate.sh
+++ tpl/superdesk/prepopulate.sh
@@ -14,9 +14,9 @@ if _missing_db; then
     python manage.py app:initialize_data $sample_data
     python manage.py users:create -u admin -p admin -e 'admin@example.com' --admin
 else
+    python manage.py app:initialize_data
     python manage.py data:upgrade
     python manage.py schema:migrate || :
-    python manage.py app:initialize_data
     python manage.py users:create -u admin -p admin -e 'admin@example.com' --admin
 fi
 {{/test_data}}
