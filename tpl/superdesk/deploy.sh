83b209a3b8e6be8abfc6b24f63754c7b587beb0f
Petr Jasek <jasekpetr@gmail.com>
2021-05-26 12:21:45 +0200

    fix superdesk client build
    use `npm run build` if available, fallback go `grunt build`

--- tpl/superdesk/deploy.sh
+++ tpl/superdesk/deploy.sh
@@ -15,8 +15,9 @@ _activate
 
 {{^develop}}
 [ -z "${grunt_build-1}" ] || (
+export NODE_OPTIONS=--max_old_space_size=2048
 cd {{repo_client}}
-time node --max-old-space-size=4096  `which grunt` build --webpack-no-progress
+time (npm run build || time `which grunt` build --webpack-no-progress)
 )
 {{/develop}}
 
