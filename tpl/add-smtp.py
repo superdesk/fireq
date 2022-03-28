255bc61ab90e20c81cc3ae5ff2a8f3d162dbd7c1
Mark Pittaway <mark.pittaway@gmail.com>
2022-03-28 16:53:59 +1100

    Improve logging of SMTP errors

--- tpl/add-smtp.py
+++ tpl/add-smtp.py
@@ -21,7 +21,7 @@ class Server(smtpd.SMTPServer, object):
     def __init__(self, path, *args, **kwargs):
         super().__init__(*args, **kwargs)
         path = Path(path)
-        path.mkdir(exist_ok=True)
+        path.mkdir(exist_ok=True, parents=True)
         self._path = path
 
     def process_message(self, peer, mailfrom, rcpttos, data, **kwargs):
@@ -38,17 +38,35 @@ class Server(smtpd.SMTPServer, object):
             email.parent.mkdir(exist_ok=True)
             email.write_text(data)
 
+    def log_info(self, message, type='info'):
+        if type in self.ignore_log_types:
+            return
+
+        try:
+            log_func = getattr(log, type)
+            if callable(log_func):
+                log_func(message)
+                return
+        except AttributeError:
+            pass
+
+        log.info('{}: {}'.format(type, message))
+
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Run an SMTP server.')
-    parser.add_argument('addr', help='addr to bind to')
-    parser.add_argument('port', type=int, help='port to bind to')
-    parser.add_argument('path', help='directory to store to')
-    args = parser.parse_args()
-
-    log.info('Starting SMTP server at {0}:{1}'.format(args.addr, args.port))
-    server = Server(args.path, (args.addr, args.port), None, decode_data=True)
     try:
+        parser = argparse.ArgumentParser(description='Run an SMTP server.')
+        parser.add_argument('addr', help='addr to bind to')
+        parser.add_argument('port', type=int, help='port to bind to')
+        parser.add_argument('path', help='directory to store to')
+        args = parser.parse_args()
+
+        log.info('Starting SMTP server at {0}:{1}'.format(args.addr, args.port))
+        server = Server(args.path, (args.addr, args.port), None, decode_data=True)
         asyncore.loop()
     except KeyboardInterrupt:
         log.info('Cleaning up')
+    except Exception as e:
+        log.exception(e)
+    finally:
+        log.info('SMTP server stopped')
