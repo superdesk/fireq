0f828709ddceacce76d41c2012847f7d7bf269d0
Mark Pittaway <mark.pittaway@gmail.com>
2022-08-17 14:54:29 +1000

    newshub: run schema_migrate on deployment

--- tpl/newsroom-app/prepopulate.sh
+++ tpl/newsroom-app/prepopulate.sh
@@ -4,3 +4,4 @@ cd {{repo}}/server
 python manage.py initialize_data
 python manage.py data_upgrade
 python manage.py create_user admin@example.com admin Admin Admin true || :
+python manage.py schema_migrate || true
