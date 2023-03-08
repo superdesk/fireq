720a0ff9a67362023b13de325e668ec2042248d6
Petr Jasek <jasekpetr@gmail.com>
2023-03-08 11:07:21 +0100

    disable client core testing to save resources

--- fireq/cli.py
+++ fireq/cli.py
@@ -35,7 +35,7 @@ Scope = namedtuple('Scope', 'name, tpldir, repo')
 scopes = [
     Scope('sd', 'superdesk', 'superdesk/superdesk'),
     Scope('sds', 'superdesk-server', 'superdesk/superdesk-core'),
-    Scope('sdc', 'superdesk-client', 'superdesk/superdesk-client-core'),
+    #Scope('sdc', 'superdesk-client', 'superdesk/superdesk-client-core'),
     Scope('sdp', 'superdesk-planning', 'superdesk/superdesk-planning'),
     Scope('sda', 'superdesk-analytics', 'superdesk/superdesk-analytics'),
     Scope('ntb', 'superdesk', 'superdesk/superdesk-ntb'),
