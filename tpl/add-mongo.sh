cfdbdedd9cd6404705ee80a5826c31befb517985
bassu <abbas.khan@sourcefabric.org>
2025-03-07 11:46:41 +0000

    avoid installing mongodb server when reseting db

--- tpl/add-mongo.sh
+++ tpl/add-mongo.sh
@@ -5,35 +5,7 @@ if ! _skip_install mongodb-org-server; then
 
     apt-get -y update
     apt-get -y install --no-install-recommends \
-#        mongodb-org-server \
         mongodb-org-shell \
         mongodb-org-tools
 fi
 
-# tune mongo
-cfg=/etc/mongod.conf
-[ -f "${cfg}.bak" ] || mv $cfg $cfg.bak
-cat <<EOF > $cfg
-storage:
-  dbPath: /var/lib/mongodb
-  journal:
-    enabled: true
-  engine: wiredTiger
-
-systemLog:
-  destination: file
-  logAppend: true
-  path: /var/log/mongodb/mongod.log
-
-net:
-  port: 27017
-  bindIp: 0.0.0.0
-EOF
-unset cfg
-systemctl enable mongod
-systemctl restart mongod
-
-# set compatibility to latest mongo
-# added by abbas
-sleep 90
-mongo admin --eval 'db.runCommand({setFeatureCompatibilityVersion: "3.4"})'
