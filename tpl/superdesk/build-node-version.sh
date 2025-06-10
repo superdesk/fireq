f424896486b85d2bd08ad6c276ef8c09f121a16c
Petr Jasek <jasekpetr@gmail.com>
2025-06-10 11:41:38 +0200

    use node 22 instead of 20

--- tpl/superdesk/build-node-version.sh
+++ tpl/superdesk/build-node-version.sh
@@ -2,4 +2,4 @@
 source ~/.nvm/nvm.sh
 
 # Install the correct Node version based on package-lock.json
-grep "lockfileVersion" package-lock.json | grep 3 && nvm use 20
+grep "lockfileVersion" package-lock.json | grep 3 && nvm use 22
