446d18e3c8f973d84f38ecb0779a8d1486cd8ff1
Petr Jasek <jasekpetr@gmail.com>
2025-06-10 12:19:58 +0200

    avoid nvm scripts in logs

--- tpl/superdesk/build-node-version.sh
+++ tpl/superdesk/build-node-version.sh
@@ -2,4 +2,9 @@
 source ~/.nvm/nvm.sh
 
 # Install the correct Node version based on package-lock.json
-grep "lockfileVersion" package-lock.json | grep 3 && nvm use 22
+if grep "lockfileVersion" package-lock.json | grep 3; then
+    set +x
+    echo "Switching to Node 22"
+    nvm use 22
+    set -x
+fi
