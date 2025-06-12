28daef99a88e22e37a903bd8432ce1268230b54b
Helmy Giacoman <helmygb@gmail.com>
2025-06-12 15:08:51 +0200

    Minor wording fix

--- tpl/superdesk/install-highcharts-server.sh
+++ tpl/superdesk/install-highcharts-server.sh
@@ -1,7 +1,7 @@
 if [ `_get_json_value analytics` == "true" ]; then
     if ! install-highcharts-server; then
-        echo "WARNING: Failed to install highcharts-server."
+        echo "WARNING: Failed to install highcharts-export-server."
         exit 0
     fi
-    echo "Highcharts server installed successfully."
+    echo "Highcharts export server installed successfully."
 fi
