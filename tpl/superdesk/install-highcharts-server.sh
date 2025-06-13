9de9bd5e96a8d7259526e3adde221d5ac5b3136c
Helmy Giacoman <helmygb@gmail.com>
2025-06-13 10:10:21 +0200

    Remove `exit 0` to avoid stopping the build
    SDESK-7591

--- tpl/superdesk/install-highcharts-server.sh
+++ tpl/superdesk/install-highcharts-server.sh
@@ -1,7 +1,7 @@
 if [ `_get_json_value analytics` == "true" ]; then
     if ! install-highcharts-server; then
         echo "WARNING: Failed to install highcharts-export-server."
-        exit 0
+    else
+        echo "Highcharts export server installed successfully."
     fi
-    echo "Highcharts export server installed successfully."
 fi
